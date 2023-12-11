d092c821b44b3d8c739225001ec62ffe8485a758
Inna949Festchuk <innabori@mail.ru>
2023-12-11 21:01:08 +0200

    -

--- 5.Django_создание_функциональных_веб-приложений/dj-homeworks/3.2-crud/stocks_products/logistic/serializers.py
+++ 5.Django_создание_функциональных_веб-приложений/dj-homeworks/3.2-crud/stocks_products/logistic/serializers.py
@@ -1,22 +1,30 @@
 from rest_framework import serializers
+from .models import Product, Stock, StockProduct
 
 
+# сериализатор для продукта
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
 
-
+                                
+# сериализатор для позиции продукта на складе
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['id', 'product', 'quantity', 'price']
 
 
+# сериализатор для склада
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
-
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'products', 'positions']
 
     def create(self, validated_data):
+        
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
 
@@ -27,17 +35,30 @@ class StockSerializer(serializers.ModelSerializer):
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
 
+        for position in positions:
+            StockProduct.objects.create(
+                stock=stock,
+                **position
+            )
         return stock
 
     def update(self, instance, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
 
-        # обновляем склад по его параметрам
-        stock = super().update(instance, validated_data)
-
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+        for position in positions:
+            StockProduct.objects.update_or_create(
+                defaults={
+                    'quantity': position['quantity'],
+                    'price': position['price']
+                },
+                product=position['product'],
+                stock=stock)
 
         return stock
--- 5.Django_создание_функциональных_веб-приложений/dj-homeworks/3.2-crud/stocks_products/logistic/views.py
+++ 5.Django_создание_функциональных_веб-приложений/dj-homeworks/3.2-crud/stocks_products/logistic/views.py
@@ -1,3 +1,5 @@
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -7,10 +9,13 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
+    pagination_class = LimitOffsetPagination
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filterset_fields = ['products']
+    pagination_class = LimitOffsetPagination
