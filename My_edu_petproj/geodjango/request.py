757eb82b4d871e43840a81a891dfe4ceb921b03d
Inna Festchuk <113055647+Inna949Festchuk@users.noreply.github.com>
2023-12-16 16:55:39 +0200

    Update request.py

--- My_edu_petproj/geodjango/request.py
+++ My_edu_petproj/geodjango/request.py
@@ -1,21 +1,5 @@
 # примеры API-запросов
 
-##from pprint import pprint
-##import requests
-##
-##url = 'http://127.0.0.1:8000/api/show_point/'
-##
-##def test_request():
-##    
-##    url = "http://127.0.0.1:8000/api/show_point/"
-##    # params = {"model": "nike123"}
-##    # headers = {"Autoriszation": "secret - token - 123"}
-##    response = requests.get(url)
-##    pprint(response)
-##
-##if __name__ == '__main__':
-##    test_request()
-
 # получение 
 import requests
 url = "http://127.0.0.1:8000/api/point/"
@@ -26,37 +10,17 @@ response.json()
 ##Ответ
 ##[
 ##    {
-##        'name': "'point2'", 'x': 954158.5, 'y': 4215137.5, 'location': 'SRID=4326;POINT (-95.33850006157502 29.72449988840415)'
+##        'name': "'point2'", 'x': '20 град 30 мин 50 с', 'y': '12 п.ш.', 'location': 'SRID=4326;POINT (-95.33850006157502 29.72449988840415)'
 ##        },
 ##    {
-##        'name': 'point3', 'x': 954159.0, 'y': 4215140.0, 'location': 'SRID=4326;POINT (-95.3384898245414 29.72452578636828)'
+##        'name': 'point3', 'x': '35 град 10 мин 10 с', 'y': '55 п.ш.', 'location': 'SRID=4326;POINT (-95.3384898245414 29.72452578636828)'
 ##        }
 ##    ]
 
-##@baseUrl = 'http://127.0.0.1:8000/api/show_point/'
-##GET {{baseUrl}}/show_point/
-##Content-Type: application/json
-###
-# # В адресной строке браузера должен быть сформирован такой запрос:
-# GET http://127.0.0.1:8000/api/show_point/
-
-###
 # создание point
-# POST {{baseUrl}}/create_point/
-# Content-Type: application/json
-
-# {
-#   "name": "point1",
-#   "x": 954158.1,
-#   "y": 4215137.1,
-# }
-
-###
-# В адресной строке браузера должен быть сформирован такой запрос:
-# POST http://127.0.0.1:8000/api/create_point/?name=point3&x=954159&y=4215140
 import requests
 url = "http://127.0.0.1:8000/api/point/"
-response = requests.post(url, data={"name": "point1", "x": 954158.1, "y": 4215137.1, "location": "SRID=4326;POINT (954158.1 4215137.1)"})
+response = requests.post(url, data={"name": "point1", "x": "-", "y": "-", "location": "SRID=4326;POINT (954158.1 4215137.1)"})
 response.status_code
 #201 - объект создан
 
