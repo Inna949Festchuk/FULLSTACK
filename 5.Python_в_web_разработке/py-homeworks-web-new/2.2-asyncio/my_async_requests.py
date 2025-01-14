2a674c91f16f8dfec1fa6b686f8eaf01a44b9abe
Inna949Festchuk <innabori@mail.ru>
2025-01-14 06:31:16 +0200

    v0.0.1

--- 5.Python_в_web_разработке/py-homeworks-web-new/2.2-asyncio/my_async_requests.py
+++ 5.Python_в_web_разработке/py-homeworks-web-new/2.2-asyncio/my_async_requests.py
@@ -1,135 +1,80 @@
-# Асинхронный подход (продолжение:выгружаем в ORM)
 import asyncio
 import aiohttp
 import datetime
-
 from more_itertools import chunked
-
-# Импортируем из models.py инициализацию и закрытие БД
-# фабрику сессий и нашу модель
 from my_created_models import init_orm, close_orm, Session, SwapiPeople
 
-MAX_COROTINS = 5
+MAX_COROUTINS = 5
+
+async def fetch_name(url, session):
+    async with session.get(url) as response:
+        json_data = await response.json()
+        return json_data.get('title') or json_data.get('name')
 
 async def get_people(people_id, session):
-    response = await session.get(f'http://swapi.py4e.com/api/people/{people_id}')
-    # response = await session.get(f'http://127.0.0.1:5000/press/{people_id}')
-    json_data = await response.json()
-    return json_data
+    async with session.get(f'http://swapi.py4e.com/api/people/{people_id}') as response:
+        json_data = await response.json()
 
-# Пишем функцию для вставки в БД значений списка result gather-а, 
-# содержащего пять вложенных JSON (это будут отдельные экземпляры ORM-модели) 
+        # Получаем названия фильмов, видов, звездолетов и транспорта
+        films = await asyncio.gather(*(fetch_name(film_url, session) for film_url in json_data.get('films', [])))
+        species = await asyncio.gather(*(fetch_name(species_url, session) for species_url in json_data.get('species', [])))
+        starships = await asyncio.gather(*(fetch_name(starship_url, session) for starship_url in json_data.get('starships', [])))
+        vehicles = await asyncio.gather(*(fetch_name(vehicle_url, session) for vehicle_url in json_data.get('vehicles', [])))
+
+        return {
+            **json_data,
+            'films': films,
+            'species': species,
+            'starships': starships,
+            'vehicles': vehicles
+        }
+
 async def insert_to_database(list_json: list[dict]):
-    # # открываем сессию
-    # async with Session() as session:
-        # objects = []
-        # for item in list_json:
-        #     # Создаем экземпляр SwapiPeople и кладем в него JSON
-        #     swapi = SwapiPeople(json=item)
-        #     objects.append(swapi)
-        # # Добавляем все объекты в сессию
-        # session.add_all)objects
-        # # Комиттимся
-        # await session.commit()
-
-    # Перерписываем этот код с помощью компрекейшн выражения
     async with Session() as session:
-        objects = [SwapiPeople(json=item) for item in list_json]
+        objects = [
+            SwapiPeople(
+                id=item.get("id"),
+                birth_year=item.get("birth_year"),
+                eye_color=item.get("eye_color"),
+                films=", ".join(item.get("films", [])),
+                gender=item.get("gender"),
+                hair_color=item.get("hair_color"),
+                height=item.get("height"),
+                homeworld=item.get("homeworld"),
+                mass=item.get("mass"),
+                name=item.get("name"),
+                skin_color=item.get("skin_color"),
+                species=", ".join(item.get("species", [])),
+                starships=", ".join(item.get("starships", [])),
+                vehicles=", ".join(item.get("vehicles", []))
+            ) for item in list_json
+        ]
+
+        # Добавляем все объекты в сессию и коммитим изменения
         session.add_all(objects)
         await session.commit()
 
-
 async def main():
-    # Инициализируем orm
     await init_orm()
 
     async with aiohttp.ClientSession() as session:
-        for coroutins_chunk in chunked(range(1, 100), MAX_COROTINS):
-            
+        tasks = []
+        for coroutins_chunk in chunked(range(1, 100), MAX_COROUTINS):
             coroutins = [get_people(i, session) for i in coroutins_chunk]
-            result = await asyncio.gather(*coroutins) # ждем завершения формирования пятерки JSON-ов
-            
-            # Вставляем result в БД
+            tasks.extend(coroutins)  # Собираем все задачи
 
-            # await insert_to_database(result)
+        # Ожидаем завершения всех задач
+        results = await asyncio.gather(*tasks)
+        # Создаем задачу для вставки данных в базу и ждем её завершения
+        await insert_to_database(results)
 
-            # Используя асинхронные инструменты оптимизируем эту часть кода
-            # await - это БЛОКИРУЮЩАЯ операция,
-            # но ведь мы можем асинхронно начать выгружать с сервера 
-            # следующую пятерку JSON-ов не дожидаясь вставки 
-            # в БД предыдущих выгруженных
-           
-            # Это можно организовать с помощью объекта пайтона Task (ЗАДАЧА):
-            # Задаем в методе create_task объект корутину insert_to_database(result)
-
-            asyncio.create_task(insert_to_database(result)) # НЕ ЖДЕМ ЗАВЕРШЕНИЯ ЗАПИСИ В БД
-            
-            # !!! ОПЕРАЦИЯ СОЗДАНИЯ КОРУТИНЫ ТЕПЕРЬ НЕ БЛОКИРУЮЩАЯ !!!
-            # !!! И ПЕРЕД asyncio.create_task() НЕТ БЛОК. ОПЕРАЦИИ await !!!
-            # !!! В ОТЛИЧАЕ ОТ await asyncio.gather() !!!
-
-            # Таким образом операция вставки в БД начинает выполняться, 
-            # но при этом мы продолжаем выполнять код дальше (начинаем 
-            # формировать сльедующую пятерку JSON-ов)
-            # НЕ ДОЖИДАЯСЬ ЗАВЕРШЕНИЯ ВЫПОЛНЕНИЯ ОПЕРАЦИИ ВСТАВКИ В БД
-
-            print(result)
-
-    # ЕЩЕ ОДНА ПРОБЛЕМА асинхронного кода - последние 5 JSON-ов не попали в БД. 
-    # Это происходит потому, что последния задача вставки в БД insert_to_database(result)
-    # не успевает завершиться, а сессия orm уже закрывается, 
-    # можно попробывать дождаться завершения выполнения 
-    # последней задачи insert_to_database(result) применением await task, 
-    
-    # await task # ждем завершения последней задачи вставки в БД insert_to_database(result)
-
-    # но тогда может не успеть завершиться
-    # предпоследняя задача insert_to_database(result), 
-    # которая по времени выполнения будет дольше  
-    
-    # !!! В этом ОСОБЕННОСТЬ АСИНХРОННОГО КОДА -
-    # мы никогда не знаем наверняка когда асинхронная функция завершиться !!!
-    # а пайтон никогда не ждет завершения функций
-
-    # КАК ГАРАНТИРОВАТЬ ВЫПОЛНЕНИЕ ЗАДАЧ?
-    tasks = asyncio.all_tasks()
-
-    # Это функция обращается к текущему event loop и забирает из него все незавершенные задачи,
-    # т.е. те, что еще выполняются сейчас и выгружает эти задачи в виде множества.
-
-    # print(tasks)
-
-    # out: 
-    # {
-    #     <Task pending name='Task-1' coro=<main() running at async_requests_4_orm.py:96> cb=[_run_until_complete_cb() at C:\Users\GIS27\AppData\Local\Programs\Python\Python38\lib\asyncio\base_events.py:184]>, 
-    #     <Task pending name='Task-239' coro=<insert_to_database() running at async_requests_4_orm.py:39>
-    #     wait_for=<Future pending cb=[BaseProtocol._on_waiter_completed(), <TaskWakeupMethWrapper object at
-    #     0x00000000056B8400>()]>>
-    # }
-
-    # Дальше мы можем снова закинуть их в гезер и выполнить с завершением конкурентно,
-    # но туда же попадет и сама асинхронная функция main() (см. out выше)
-    # и наше приложение зацикливается - мы получаем РЕКУРСИЮ
-    
-    # Поэтому сначала получаем текущую задачу main()
-    current_task = asyncio.current_task()
-    # и далее из множества задач tasks удаляю ее
-    tasks.remove(current_task)
-
-    # print(tasks)
-    # out:
-    # {
-    #     <Task pending name='Task-239' coro=<insert_to_database() running at async_requests_4_orm.py:39> wait_for=<Future pending cb=[BaseProtocol._on_waiter_completed(), <TaskWakeupMethWrapper object at 0x00000000056BA460>()]>>
-    # }
-
-    # Теперь в гезере мы конкурентно выполняем с завершением  
-    # все текущие невыполненные задачи кроме main()
-    await asyncio.gather(*tasks)
-           
-    # Закрываем сессию orm
+    # Закрываем ORM
     await close_orm()
 
 start = datetime.datetime.now()
-asyncio.run(main()) 
+asyncio.run(main())
 print(datetime.datetime.now() - start)
+
+
+
 
--- 5.Python_в_web_разработке/py-homeworks-web-new/2.2-asyncio/my_created_models.py
+++ 5.Python_в_web_разработке/py-homeworks-web-new/2.2-asyncio/my_created_models.py
@@ -1,74 +1,54 @@
 import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy import Integer, String
 
-# Используем асинхронный движок для работы с БД
-from sqlalchemy.ext.asyncio import create_async_engine
-# Используем фабрику асинхронных сессий
-from sqlalchemy.ext.asyncio import async_sessionmaker
-# Используем базовый класс для создания моделей
-from sqlalchemy.orm import DeclarativeBase
-# Импортируем класс АИНХРОННЫЕ АТРИБУТЫ
-from sqlalchemy.ext.asyncio import AsyncAttrs
-
-from sqlalchemy.orm import mapped_column, Mapped # колонки и типизация
-# Импортируем типы данных
-from sqlalchemy import Integer, JSON
-
+# Переменные окружения для подключения к базе данных
 POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "admin")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "test")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "127.0.0.1")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 
-# DSN строка отлична для работы в АСИНХРОННОМ РЕЖИМЕ
+# DSN строка для работы с базой данных
 PG_DSN = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@" \
-    f"{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+          f"{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 # Создаем асинхронный движок для работы с БД
 engine = create_async_engine(PG_DSN)
 
 # Создаем фабрику асинхронных сессий
 Session = async_sessionmaker(bind=engine, expire_on_commit=False)
-# expire_on_commit=False - отключаем закрытие сессии после первого же коммита
 
 # Создаем базовый класс для создания моделей
 class Base(DeclarativeBase, AsyncAttrs):
-    # мы хотим чтобы некоторые атрибуты нашей модели
-    # стали асинхронными поэтому делаем
-    # множественное наследование от класса AsyncAttrs
     pass
 
 # Создаем нашу модель
 class SwapiPeople(Base):
-    __tablename__ = "swapi_people"
+    __tablename__ = "home_task_swapi"
 
-    # Смотрим наши JSON (те которые мы получаем при обращении на сервак)
-    # Задаем поля сопоставляя питоновский тип с постгрессовским
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    json: Mapped[dict] = mapped_column(JSON, nullable=True) # nullable=True - обязательное поле
-
-# Запросы к БД будут уходить асинхронно,
-# соответственно они должны быть внутри event loop
+    birth_year: Mapped[str] = mapped_column(String, nullable=True)
+    eye_color: Mapped[str] = mapped_column(String, nullable=True)
+    films: Mapped[str] = mapped_column(String, nullable=True)
+    gender: Mapped[str] = mapped_column(String, nullable=True)
+    hair_color: Mapped[str] = mapped_column(String, nullable=True)
+    height: Mapped[str] = mapped_column(String, nullable=True)
+    homeworld: Mapped[str] = mapped_column(String, nullable=True)
+    mass: Mapped[str] = mapped_column(String, nullable=True)
+    name: Mapped[str] = mapped_column(String, nullable=True)
+    skin_color: Mapped[str] = mapped_column(String, nullable=True)
+    species: Mapped[str] = mapped_column(String, nullable=True)
+    starships: Mapped[str] = mapped_column(String, nullable=True)
+    vehicles: Mapped[str] = mapped_column(String, nullable=True)
 
 # Инициализируем ORM
 async def init_orm():
-    # для этого из engine с помощью менеджера контекста with
-    # вырвем одно подключение и в нем с помощью 
-    # "запускатора" run.sync() выполним запрос в базу на создание таблиц,
-    # которые привязаны к модели, что создаст таблицу "swapi_people"
     async with engine.begin() as connect:
-        # Решаем проблему НАСЛОЕНИЯ
-        # Удаляем предыдущие записи
-        await connect.run_sync(Base.metadata.drop_all)     
-
+        await connect.run_sync(Base.metadata.drop_all)
         await connect.run_sync(Base.metadata.create_all)
 
 # Функция, закрывающая ORM
 async def close_orm():
     await engine.dispose()
-
-
-
-
-
-
-
