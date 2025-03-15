5b655f1c0b984429b10211042dedccc47bde0909
Inna949Festchuk <innabori@mail.ru>
2025-03-16 01:53:23 +0200

    -

--- 5.Python_в_web_разработке/lesson_5.2.12/django_celery_example-master/django_celery_example/settings.py
+++ 5.Python_в_web_разработке/lesson_5.2.12/django_celery_example-master/django_celery_example/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-APPEND_SLASH = False
+APPEND_SLASH = False 
 
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_BROKER_TRANSPORT = 'redis'
